f730ed5661355ded90b2fcf92db9ec2ce2eeddad
Yuki <yuki_yamakawa@kokuyo.com>
2024-08-17 13:28:16 +0900

    .envファイルの削除

--- frontend/src/app/fetchData.js
+++ frontend/src/app/fetchData.js
@@ -1,5 +1,5 @@
 export default async function fetchData() {
-    const res = await fetch('http://127.0.0.1:8000/mentor/1/home', { cache: "no-cache" });
+    const res = await fetch(process.env.API_ENDPOINT + '/mentor/1/home', { cache: "no-cache" });
 
     if (!res.ok) {
       throw new Error('Failed to fetch customers');
